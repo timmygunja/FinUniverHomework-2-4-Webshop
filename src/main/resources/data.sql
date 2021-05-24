a693bff32333b36f441c039cc51a6f9f50520c52
timmygunja <t.urusov14@mail.ru>
2021-05-24 12:19:18 +0300

    Minor fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,12 @@ INSERT INTO cart(profile_id)
 VALUES (1);
 
 INSERT INTO product(name, description, price)
-VALUES ('name1', 'description1', 1000);
+VALUES ('Prod one', 'some description', 1500),
+       ('Prod two', 'some description', 500),
+       ('Prod three', 'some description', 2000),
+       ('Prod four', 'some description', 1000),
+       ('Prod five', 'some description', 3500),
+       ('Prod six', 'some description', 4000);
 
 INSERT INTO cart_product(product_id, quantity)
 VALUES (1, 5);
